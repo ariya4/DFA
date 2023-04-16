6664bbb5b496d95a0ec6dc88b7556cb6b732e538
Ariya <bahraini913.ab@gmail.com>
2023-04-16 16:51:36 +0430

    bug fixed

--- DFA Manual.py
+++ DFA Manual.py
@@ -5,21 +5,21 @@ Q = ['q0', 'q1', 'q2']  # ! States
 E = ['0', '1']  # ! Alphabet
 F = ['q0', 'q2']  # ! final States
 s = [['q1', 'q2'], ['q1', 'q0'], ['q1', 'q0']]  # ! Pattern
-route = []
 
 
 def AAA(q, end):
-    route.append(s[Q.index(q)][E.index(end)])
+    natige.append(s[Q.index(q)][E.index(end)])
     return s[Q.index(q)][E.index(end)]
 
 
 while 1 == 1:
-    route = []
+    natige = [Q[0]]
     start = Q[0]
     reza = (input("what's the string bro: "))
     for i in reza:
-        star = AAA(start, i)
-    if route[-1] in F:
+        start = AAA(start, i)
+    if natige[-1] in F:
         print('Accepted')
     else:
         print('Not Accepted')
+    print(natige)
